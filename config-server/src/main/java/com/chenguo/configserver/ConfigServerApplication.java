513520ce7c24dd496206f09f74aa77a3c6e656e2
chenguo <962332430@qq.com>
2019-06-29 16:15:04 +0800

    [update]调整配置文件位置

--- config-server/src/main/java/com/chenguo/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/chenguo/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.chenguo.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
